418993def4b68515c17041dc6d9104636b1b8571
technologyordie <adam@fullspansolutions.com>
2018-04-06 20:42:16 -0400

    MySQL Backup Script

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Replace USERNAME, PASSWORD and MYDB with the info from your environment
 cd /var/db_backups
 /usr/bin/mysqldump --opt -u USERNAME -pPASSWORD MYDB > $(date +%Y_%m_%d_%T_MYDB.sql)
 find /var/db_backups -name "20*" -mtime +30 -exec rm -f {} \;
